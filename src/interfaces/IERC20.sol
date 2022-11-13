3f9c9cbb61aa96248e815ea2f98f77b9a0197bf8
JetJadeja <JetJadeja@gmail.com>
2022-11-12 20:00:54 -0800

    Fix interfaces

--- src/interfaces/IERC20.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.8.17;
-
-interface IERC20 {
-    function transfer(address to, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
--- src/interfaces/IFlashBorrower.sol
+++ src/interfaces/IFlashBorrower.sol
@@ -1,1 +1,9 @@
 pragma solidity 0.8.17;
+
+/// @title Flash Borrower Interface
+/// @notice Interface for flash loan borrowers.
+interface IFlashBorrower {
+    /// @notice The function that is called by the flash lender contract when funds are borrowed.
+    /// @param amount The amount of funds that are borrowed.
+    function executeOnFlashLoan(uint256 amount) external;
+}
