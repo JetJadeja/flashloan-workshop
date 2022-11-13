41148c8fa6d18450f2f32eed419d493578d3cb04
JetJadeja <JetJadeja@gmail.com>
2022-11-12 19:19:29 -0800

    add basic contract code

---
+++ src/SimpleFlashLender.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.17;
+
+import "src/interfaces/IFlashBorrower.sol";
+import "/interfaces/IERC20.sol";
+
+/// @title Simple Flash Lender
+/// @author Jet Jadeja <jet@pentagon.xyz>
+/// @notice This contract is a simple flash lender that allows anyone to deposit and borrow funds.
+contract SimpleFlashLander {
+    /*///////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Set up the flash lender contract.
+    constructor(IERC20) 
+
+    /*///////////////////////////////////////////////////////////////
+                            LENDER INTERFACES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice
+    function deposit() external {}
+}
--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -1,1 +1,6 @@
 pragma solidity 0.8.17;
+
+interface IERC20 {
+    function transfer(address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
