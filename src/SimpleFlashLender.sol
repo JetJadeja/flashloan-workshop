88395f1515cbd0d8dbe0e0db06fd273a4be338de
JetJadeja <JetJadeja@gmail.com>
2022-11-12 20:05:07 -0800

    Implement SimpleFlashLender code

--- src/SimpleFlashLender.sol
+++ src/SimpleFlashLender.sol
@@ -1,24 +1,53 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.17;
 
+// Interfaces
 import "src/interfaces/IFlashBorrower.sol";
-import "/interfaces/IERC20.sol";
+import "solmate/tokens/ERC20.sol";
+
+// Libraries
+import "solmate/utils/SafeTransferLib.sol";
 
 /// @title Simple Flash Lender
 /// @author Jet Jadeja <jet@pentagon.xyz>
-/// @notice This contract is a simple flash lender that allows anyone to deposit and borrow funds.
-contract SimpleFlashLander {
+/// @notice Simple flash lender contract that allows liquidity providers to deposit and withdraw funds.
+contract SimpleFlashLender {
+    using SafeTransferLib for ERC20;
+
+    /*///////////////////////////////////////////////////////////////
+                                CONSTANTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The address of the ERC20 contract.
+    ERC20 public immutable TOKEN;
+
     /*///////////////////////////////////////////////////////////////
                                CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
     /// @notice Set up the flash lender contract.
-    constructor(IERC20) 
+    constructor(ERC20 _TOKEN) {
+        // Set our token to the correct ERC20 address.
+        TOKEN = _TOKEN;
+    } 
 
     /*///////////////////////////////////////////////////////////////
-                            LENDER INTERFACES
+                            LENDING INTERFACE
     //////////////////////////////////////////////////////////////*/
 
-    /// @notice
-    function deposit() external {}
+    /// @notice Borrow funds from the flash lender contract.
+    function borrow(uint256 amount, IFlashBorrower borrower) external {
+        // Store the current balance of the contract.
+        uint256 balance = TOKEN.balanceOf(address(this));
+
+        // Transfer the tokens from the contract to the borrower.
+        TOKEN.safeTransfer(address(borrower), amount);
+
+
+        // Call the borrower's executeOnFlashLoan function.
+        borrower.executeOnFlashLoan(amount);
+
+        // Ensure that the tokens have been returned to the contract.
+        require(TOKEN.balanceOf(address(this)) >= balance, "Borrower did not return funds");
+    }
 }
