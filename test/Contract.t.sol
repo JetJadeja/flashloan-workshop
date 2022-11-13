c3ff5a4995966722e9b7967ed5fd4edfdeaca7fd
JetJadeja <JetJadeja@gmail.com>
2022-11-12 19:19:17 -0800

    Add interfaces

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
-import "forge-std/Test.sol";
+// import "forge-std/Test.sol";
 
-import "src/Contract.sol";
+// import "src/Contract.sol";
 
-contract TestContract is Test {
-    Contract c;
+// contract TestContract is Test {
+//     Contract c;
 
-    function setUp() public {
-        c = new Contract();
-    }
+//     function setUp() public {
+//         c = new Contract();
+//     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
+//     function testBar() public {
+//         assertEq(uint256(1), uint256(1), "ok");
+//     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
-    }
-}
+//     function testFoo(uint256 x) public {
+//         vm.assume(x < type(uint128).max);
+//         assertEq(x + x, x * 2);
+//     }
+// }
