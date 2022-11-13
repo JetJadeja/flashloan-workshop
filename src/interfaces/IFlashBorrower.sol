8e62b666cef1ed4faf2add49930b5ef9d850f816
JetJadeja <JetJadeja@gmail.com>
2022-11-12 20:50:49 -0800

    Add total parameter to flash borrower

--- src/interfaces/IFlashBorrower.sol
+++ src/interfaces/IFlashBorrower.sol
@@ -7,6 +7,8 @@ import "solmate/tokens/ERC20.sol";
 /// @notice Interface for flash loan borrowers.
 interface IFlashBorrower {
     /// @notice The function that is called by the flash lender contract when funds are borrowed.
+    /// @param token The address of the ERC20 token that is being borrowed.
     /// @param amount The amount of funds that are borrowed.
-    function executeOnFlashLoan(ERC20 token, uint256 amount) external;
+    /// @param total The amount that must be returned (including fees).
+    function executeOnFlashLoan(ERC20 token, uint256 amount, uint256 total) external;
 }
